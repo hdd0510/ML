4824edf57975997cfc2ebc22c059294bb8e978c8
hdd0510 <hoangdungit510@gmail.com>
2022-12-07 13:07:40 +0700

    update

---
+++ ML-code/nháp.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+my_data = pd.read_csv("data/drug200.csv")
+
+my_data.head()
---
+++ ML-code/nháp1.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+my_data = pd.read_csv("data/drug200.csv", delimiter=",")
+
+print(my_data.head())
